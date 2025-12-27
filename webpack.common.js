9983e69806daad93b03c484521ab4160a6a10fdc
Aritra <158503009+devritra@users.noreply.github.com>
2025-12-27 20:08:16 +0530

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line no-undef
+const path = require('path');
+// eslint-disable-next-line no-undef
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// eslint-disable-next-line no-undef
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        // eslint-disable-next-line no-undef
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
