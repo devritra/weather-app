a4c41bcab345d78c48352d8279c072082599ed1c
Aritra Saha <158503009+devritra@users.noreply.github.com>
2026-01-02 00:23:21 +0530

    Created the module displayWeatherData
    this module take the response and display the appropiate data in the correct place on the data page

--- src/fetchAndLoad.js
+++ src/fetchAndLoad.js
@@ -1,10 +1,12 @@
+import { displayWeatherData } from "./displayWeatherData";
 import { fetchWeatherData } from "./fetchWeatherData";
 import { loadDataPage } from "./loadDataPage";
 
 export async function fetchAndLoad(location) {
     // function that shows a loading screen
-    // const response = await fetchWeatherData(location);
+    const response = await fetchWeatherData(location);
     // function that removes the loading screen
-    // console.log(response);
+    console.log(response);
     loadDataPage();
+    displayWeatherData(response);
 }
