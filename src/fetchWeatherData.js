b56d9e592855f0bb783f4a2d2bd73c9f67ccdb00
Aritra Saha <158503009+devritra@users.noreply.github.com>
2026-01-01 17:26:07 +0530

    Finalized the query
    set an width to the home search button
    
    added one more data card fpr precipitation cover

--- src/fetchWeatherData.js
+++ src/fetchWeatherData.js
@@ -1,7 +1,7 @@
 import { showError } from "./showError";
 
 export async function fetchWeatherData(location){
-    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next7days?unitGroup=metric&elements=add%3Aaqieur%2Cremove%3AdatetimeEpoch%2Cremove%3Adew%2Cremove%3Afeelslikemax%2Cremove%3Afeelslikemin%2Cremove%3Aicon%2Cremove%3Amoonphase%2Cremove%3Aname%2Cremove%3Aprecipcover%2Cremove%3Apreciptype%2Cremove%3Apressure%2Cremove%3Asolarenergy%2Cremove%3Asolarradiation%2Cremove%3Astations%2Cremove%3Auvindex&include=days&key=REA7E7U983BWPKC3YC7S55S25&contentType=json`;
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/today?unitGroup=metric&elements=add%3Aaqius%2Cremove%3Adew%2Cremove%3Afeelslikemax%2Cremove%3Afeelslikemin%2Cremove%3Amoonphase%2Cremove%3Apressure%2Cremove%3Asevererisk%2Cremove%3Asolarenergy%2Cremove%3Asolarradiation%2Cremove%3Astations%2Cremove%3Auvindex&include=days&key=REA7E7U983BWPKC3YC7S55S25&contentType=json`;
     try{
         const response = await fetch(url);
         if(response.ok){
